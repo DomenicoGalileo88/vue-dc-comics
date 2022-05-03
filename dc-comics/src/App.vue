e5e8117d2de3821e4b78100bc8e3c2a13ee3644e
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-03 15:00:17 +0200

    initial commit

---
+++ dc-comics/src/App.vue
@@ -0,0 +1,57 @@
+/* 
+Descrizione:
+Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+ */
+
+<template>
+  <div id="app">
+    <div class="container">
+      <SiteHeader />
+
+    <main class="site_main"></main>
+    <!-- /.site_main -->
+
+    <footer class="site_footer"></footer>
+    <!-- /.site_footer -->
+    </div>
+  </div>
+</template>
+
+<script>
+import SiteHeader from '@/components/HeaderComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  /* commons */
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  img{
+    max-width: 100%;
+  }
+
+  .container{
+    width: 1200px;
+    margin: 2rem auto;
+  }
+}
+
+
+</style>
---
+++ dc-comics/src/components/HeaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <header class="site_header">
+      <div class="logo">
+        <img src="@/assets/img/dc-logo.png" alt="">
+      </div>
+      <!-- /.logo -->
+
+      <nav class="navbar">
+        <ul>
+          <li>CHARACTERS</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GAMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
+      </nav>
+      <!-- /.navbar -->
+    </header>
+    <!-- /.site_header -->
+</template>
+
+<script>
+export default {
+    name: 'SiteHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  nav{
+    ul{
+      display: flex;
+
+      li{
+        list-style: none;
+        padding: 1rem;
+      }
+    }
+  }
+}
+
+</style>
