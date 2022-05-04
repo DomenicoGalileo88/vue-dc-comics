3df78b342854d714b4ac4bf2ec4ed8294bc29d18
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-04 17:38:49 +0200

    Add partials

--- dc-comics/src/components/HeaderComponent.vue
+++ dc-comics/src/components/HeaderComponent.vue
@@ -1,107 +1,72 @@
 <template>
-    <header class="site_header">
-      <div class="container">
-        
-
+  <header class="site_header">
+    <div class="container">
       <nav class="navbar">
-          <div class="row">
-              <div class="col-6 py-4">
-                <div class="logo">
-                    <img src="@/assets/img/dc-logo.png" alt="">
-                </div>
-                 <!-- /.logo -->
-              </div>
-              <div class="col-6">
-                <ul>
-                    <li v-for="(link, index) in nav_links" :key="index">
-                        <a href="#">{{link.nome}}</a>
-                    </li>
-                </ul>
-              </div>
+        <div class="row">
+          <div class="col-6 py-4">
+            <div class="logo">
+              <img src="@/assets/img/dc-logo.png" alt="" />
+            </div>
+            <!-- /.logo -->
           </div>
+          <div class="col-6">
+            <ul>
+              <li v-for="(link, index) in nav_links" :key="index">
+                <a href="#">{{ link.nome }}</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
       <!-- /.navbar -->
-      </div>
-      <!-- /.container -->
-    </header>
-    <!-- /.site_header -->
+    </div>
+    <!-- /.container -->
+  </header>
+  <!-- /.site_header -->
 </template>
 
 <script>
 export default {
-    name: 'SiteHeader', 
-    data() {
-        return {
-            nav_links: [
-                {
-                    nome: 'CHARACTERS'
-                },
-                {
-                    nome: 'COMMICS'
-                },
-                {
-                    nome: 'MOVIES'
-                },
-                {
-                    nome: 'TV'
-                },
-                {
-                    nome: 'GAMES'
-                },
-                {
-                    nome: 'COLLECTIBLES'
-                },
-                {
-                    nome: 'VIDEOS'
-                },
-                {
-                    nome: 'FANS'
-                },
-                {
-                    nome: 'NEWS'
-                },
-                {
-                    nome: 'SHOP'
-                },
-            ]
-        }
-    }
-}
+  name: "SiteHeader",
+  data() {
+    return {
+      nav_links: [
+        {
+          nome: "CHARACTERS",
+        },
+        {
+          nome: "COMMICS",
+        },
+        {
+          nome: "MOVIES",
+        },
+        {
+          nome: "TV",
+        },
+        {
+          nome: "GAMES",
+        },
+        {
+          nome: "COLLECTIBLES",
+        },
+        {
+          nome: "VIDEOS",
+        },
+        {
+          nome: "FANS",
+        },
+        {
+          nome: "NEWS",
+        },
+        {
+          nome: "SHOP",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-header{
-        .container{
-            a{
-                display: flex;
-                align-items: center;
-                color: black;
-                text-decoration: none;
-                height: 100%;
-                &:hover{
-                    color: $dc-primary;
-                }
-            }
-              nav{
-                  padding: 0;
-                ul{
-                    height: 100%;
-                    display: flex;
-                  li{
-                    font-size: 0.9rem;
-                    font-weight: 600;
-                    list-style: none;
-                    padding: 0.5rem;
-                    &:hover{
-                        border-bottom: 5px solid $dc-primary;
-                    }
-                    
-                  }
-                }
-              }
-              
-             }
-}
-
+@import "@/assets/scss/partials/_header.scss";
 </style>
